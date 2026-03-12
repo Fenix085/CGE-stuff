86a93677ab12e62a77577fded12875e51ae670c6
Volodymyr <79228320+Fenix085@users.noreply.github.com>
2026-03-12 15:35:21 +0100

    add smth to Globals
    probably useful

--- TheNewBeginning/MainEngine/Global/Globals.cs
+++ TheNewBeginning/MainEngine/Global/Globals.cs
@@ -1,1 +1,14 @@
+using System;
+using Microsoft.Xna.Framework;
+
 namespace MainEngine.Global;
+
+public class Globals
+{
+    public static float ElapsedTime = 0f;
+    public static float FixedUpdateMultiplier = 0f;
+    public static GameTime GameTime;
+    public static float NextTickTime = 0;
+    public static float FixedUpdateAlpha;
+    internal static TimeSpan FixedUpdateRate;
+}
