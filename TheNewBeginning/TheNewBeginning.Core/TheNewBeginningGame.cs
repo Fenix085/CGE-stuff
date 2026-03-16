bdb311c820309bdfc3d3580377f018ae1c1f3783
Grompesto <lordgromp0806@gmail.com>
2026-03-16 12:10:51 +0100

    Player

--- TheNewBeginning/TheNewBeginning.Core/TheNewBeginningGame.cs
+++ TheNewBeginning/TheNewBeginning.Core/TheNewBeginningGame.cs
@@ -14,11 +14,8 @@ namespace TheNewBeginning.Core;
 
 public class TheNewBeginningGame : HQ
 {
-    // Defines the slime animated sprite.
-    private AnimatedSprite _player;
-
-    private Health _playerHP;
-
+    
+    private Player _player;
     private Camera _camera;
 
     // Defines the bat animated sprite.
@@ -36,12 +33,9 @@ public class TheNewBeginningGame : HQ
     private Sprite _agentSprite;
     private AgentConfig _agentConfig;
     private List<ForceSource> _forceSources;
-    // Tracks the position of the player.
-    private Vector2 _playerPosition;
 
     private Vector2 _enemyPosition;
     // Speed multiplier when moving.
-    private const float MOVEMENT_SPEED = 5.0f;
 
     public TheNewBeginningGame() : base("The New Beginning", 1280, 720, false)
     {
@@ -51,7 +45,6 @@ public class TheNewBeginningGame : HQ
     protected override void Initialize()
     {
         _camera = new Camera();
-        _playerHP = new Health(3);
         _enemyHP = new Health(3);
         _enemyDead = false;
 
@@ -64,16 +57,18 @@ public class TheNewBeginningGame : HQ
         TextureAtlas atlas = TextureAtlas.FromFile(Content, "images/atlas-definition.xml");
 
         // Create the player animated sprite from the atlas.
-        _player = atlas.CreateAnimatedSprite("player-animation");
-        _player.Scale = new Vector2(4.0f, 4.0f);
-        _player.CenterOrigin();
+        AnimatedSprite playerSprite = atlas.CreateAnimatedSprite("player-animation");
+        playerSprite.Scale = new Vector2(4f);
+        playerSprite.CenterOrigin();
+
+        _player = new Player(playerSprite, Vector2.Zero, 3);
 
         // Create the enemy animated sprite from the atlas.
         _enemy = atlas.CreateAnimatedSprite("enemy-animation");
         _enemy.Scale = new Vector2(4.0f, 4.0f);
         _enemy.CenterOrigin();
 
-        _enemyPosition = new Vector2(_player.Width + 10, 0);
+        _enemyPosition = new Vector2(playerSprite.Width + 10, 0);
 
         // Set up the agent sprite using the first Orc frame.
         _agentSprite = atlas.CreateSprite("enemy-1");
@@ -114,7 +109,7 @@ public class TheNewBeginningGame : HQ
 
     protected override void Update(GameTime gameTime)
     {
-        // Update the player animated sprite.
+        
         _player.Update(gameTime);
 
         // Update the enemy animated sprite.
@@ -122,7 +117,7 @@ public class TheNewBeginningGame : HQ
 
         // Build force sources for this frame.
         _forceSources.Clear();
-        _forceSources.Add(new ForceSource(_playerPosition, 250f, -100f));
+        _forceSources.Add(new ForceSource(_player.Position, 250f, -100f));
         // Add projectiles, obstacles, lures, etc.:
         // _forceSources.Add(new ForceSource(projectilePos, 60f, -15f));  // repels
         // _forceSources.Add(new ForceSource(lurePos, 120f, 5f));       // attracts
@@ -134,20 +129,11 @@ public class TheNewBeginningGame : HQ
 
         // Check for mouse input and handle it.
         CheckMouseInput();
-        // Check for keyboard input and handle it.
-        CheckKeyboardInput();
 
-        // Check for gamepad input and handle it.
-        CheckGamePadInput();
-
-        _camera.Pos = _playerPosition;
+        _camera.Pos = _player.Position;
 
         // Creating a bounding circle for the player sprite to use for collision checks.
-        Circle playerBounds = new Circle(
-            (int)(_playerPosition.X + (_player.Width * 0.1f)),
-            (int)(_playerPosition.Y + (_player.Height * 0.1f)),
-            (int)(_player.Width * 0.1f)
-        );
+        Circle playerBounds = _player.GetBounds();
         
         // Creating a bounding circle for the enemy sprite to use for collision checks.
         
@@ -160,13 +146,13 @@ public class TheNewBeginningGame : HQ
         
         
 
-        if (enemyBounds.Intersects(playerBounds))
+        if (enemyBounds.Intersects(_player.GetBounds()))
         {
             
             // Divide the width  and height of the screen into equal columns and
-            // rows based on the width and height of the bat.
-            int totalColumns = GraphicsDevice.PresentationParameters.BackBufferWidth / (int)_player.Width;
-            int totalRows = GraphicsDevice.PresentationParameters.BackBufferHeight / (int)_player.Height;
+            // rows based on the width and height of the enemy.
+            int totalColumns = GraphicsDevice.PresentationParameters.BackBufferWidth / (int)_player.Sprite.Width;
+            int totalRows = GraphicsDevice.PresentationParameters.BackBufferHeight / (int)_player.Sprite.Height;
 
             // Choose a random row and column based on the total number of each
             int column = Random.Shared.Next(0, totalColumns);
@@ -174,10 +160,10 @@ public class TheNewBeginningGame : HQ
 
             // Change the bat position by setting the x and y values equal to
             // the column and row multiplied by the width and height.
-            _playerPosition = new Vector2(column * _player.Width, row * _player.Height);
+            _player.Position = new Vector2(column * _player.Sprite.Width, row * _player.Sprite.Height);
             
-            _playerHP.TakeDamage(2);
-            if(_playerHP.IsDead)
+            _player.Health.TakeDamage(2);
+            if(_player.Health.IsDead)
             {
                 Exit();
             }
@@ -213,104 +199,18 @@ public class TheNewBeginningGame : HQ
                     Matrix.Invert(_camera.get_transformation(GraphicsDevice))
                 );
 
-            Vector2 direction = mouseWorld - _playerPosition;
+            Vector2 direction = mouseWorld - _player.Position;
             direction.Normalize();
 
             Projectile projectile = new Projectile
             {
-                Position = _playerPosition,
+                Position = _player.Position,
                 Direction = direction
             };
             _projectiles.Add(projectile);
         }
     }
-    private void CheckKeyboardInput()
-    {
-        // If the space key is held down, the movement speed increases by 1.5
-        float speed = MOVEMENT_SPEED;
-        if (Input.Keyboard.IsKeyDown(Keys.Space))
-        {
-            speed *= 1.5f;
-        }
 
-        // If the W or Up keys are down, move the player up on the screen.
-        if (Input.Keyboard.IsKeyDown(Keys.W) || Input.Keyboard.IsKeyDown(Keys.Up))
-        {
-            _playerPosition.Y -= speed;
-        }
-
-        // if the S or Down keys are down, move the player down on the screen.
-        if (Input.Keyboard.IsKeyDown(Keys.S) || Input.Keyboard.IsKeyDown(Keys.Down))
-        {
-            _playerPosition.Y += speed;
-        }
-
-        // If the A or Left keys are down, move the player left on the screen.
-        if (Input.Keyboard.IsKeyDown(Keys.A) || Input.Keyboard.IsKeyDown(Keys.Left))
-        {
-            _playerPosition.X -= speed;
-        }
-
-        // If the D or Right keys are down, move the player right on the screen.
-        if (Input.Keyboard.IsKeyDown(Keys.D) || Input.Keyboard.IsKeyDown(Keys.Right))
-        {
-            _playerPosition.X += speed;
-        }
-    }
-
-
-    private void CheckGamePadInput()
-    {
-        GamePadInfo gamePadOne = Input.GamePads[(int)PlayerIndex.One];
-
-        // If the A button is held down, the movement speed increases by 1.5
-        // and the gamepad vibrates as feedback to the player.
-        float speed = MOVEMENT_SPEED;
-        if (gamePadOne.IsButtonDown(Buttons.A))
-        {
-            speed *= 1.5f;
-            gamePadOne.SetVibration(1.0f, TimeSpan.FromSeconds(1));
-        }
-        else
-        {
-            gamePadOne.StopVibration();
-        }
-
-        // Check thumbstick first since it has priority over which gamepad input
-        // is movement.  It has priority since the thumbstick values provide a
-        // more granular analog value that can be used for movement.
-        if (gamePadOne.LeftThumbStick != Vector2.Zero)
-        {
-            _playerPosition.X += gamePadOne.LeftThumbStick.X * speed;
-            _playerPosition.Y -= gamePadOne.LeftThumbStick.Y * speed;
-        }
-        else
-        {
-            // If DPadUp is down, move the player up on the screen.
-            if (gamePadOne.IsButtonDown(Buttons.DPadUp))
-            {
-                _playerPosition.Y -= speed;
-            }
-
-            // If DPadDown is down, move the player down on the screen.
-            if (gamePadOne.IsButtonDown(Buttons.DPadDown))
-            {
-                _playerPosition.Y += speed;
-            }
-
-            // If DPapLeft is down, move the player left on the screen.
-            if (gamePadOne.IsButtonDown(Buttons.DPadLeft))
-            {
-                _playerPosition.X -= speed;
-            }
-
-            // If DPadRight is down, move the player right on the screen.
-            if (gamePadOne.IsButtonDown(Buttons.DPadRight))
-            {
-                _playerPosition.X += speed;
-            }
-        }
-    }
 
     protected override void Draw(GameTime gameTime)
     {
@@ -324,7 +224,7 @@ public class TheNewBeginningGame : HQ
         );
 
         // Draw the player sprite.
-        _player.Draw(SpriteBatch, _playerPosition);
+        _player.Draw(SpriteBatch);
 
         // Draw the enemy sprite 10px to the right of the player.
         if (!_enemyDead)
