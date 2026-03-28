b78da81719054a9e681efbc0c43db408154b5a72
Grompesto <lordgromp0806@gmail.com>
2026-03-28 12:58:24 +0100

    1 test

--- TheNewBeginning/TheNewBeginning.Core/Scenes/GameScene.cs
+++ TheNewBeginning/TheNewBeginning.Core/Scenes/GameScene.cs
@@ -50,6 +50,36 @@ private AnimatedButton _resumeButton;
 private SoundEffect _uiSoundEffect;
 private TextureAtlas _atlas;
 
+private Dictionary<Vector2, int> test_map;
+    private Dictionary<Vector2, int> LoadMap(string filepath)
+    {
+        Dictionary<Vector2, int> result = new();
+
+        StreamReader reader = new(filepath);
+
+        int y = 0;
+        string line;
+        while((line = reader.ReadLine()) != null)
+        {
+            string[] items = line.Split(',');
+
+            for (int x = 0; x < items.Length; x++)
+            {
+                if (int.TryParse(items[x], out int value))
+                {
+                    if (value > -1)
+                    {
+                        result[new Vector2(x,y)] = value;
+                    }
+                }
+            }
+
+            y++;
+        }
+        return result;
+    }
+
+    public 
 
     public override void Initialize()
     {
