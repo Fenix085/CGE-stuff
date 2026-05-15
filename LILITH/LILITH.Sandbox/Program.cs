86ca492b423525e2d1c6e95fb61512902366c529
Volodymyr <79228320+Fenix085@users.noreply.github.com>
2026-05-15 10:56:51 +0200

    add Sandbox project for LILITH

---
+++ LILITH/LILITH.Sandbox/Program.cs
@@ -0,0 +1,2 @@
+﻿using var game = new LILITH.Sandbox.SandboxGame();
+game.Run();
---
+++ LILITH/LILITH.Sandbox/SandboxGame.cs
@@ -0,0 +1,34 @@
+﻿using MainEngine;
+using MainEngine.Scenes;
+using Microsoft.Xna.Framework;
+
+namespace LILITH.Sandbox;
+
+public sealed class SandboxGame : HQ
+{
+    public SandboxGame() : base("Sandbox", 1280, 720, false){ }
+
+    protected override void Initialize()
+    {
+        base.Initialize();
+        ChangeScene(new SandboxScene());
+    }
+}
+
+public sealed class SandboxScene : Scene
+{
+    public override void LoadContent()
+    {
+        
+    }
+
+    public override void Update(GameTime gameTime)
+    {
+        
+    }
+
+    public override void Draw(GameTime gameTime)
+    {
+        
+    }
+}
